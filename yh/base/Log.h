f89d24f3ab97ed183f4e94cb9c44358f1b702b3d
trarck <trarck@gmail.com>
2014-03-21 18:21:28 +0800

    新的lib结构

---
+++ yh/base/Log.h
@@ -0,0 +1,104 @@
+#ifndef YH_BASE_OBJECT_H_
+#define YH_BASE_OBJECT_H_
+
+#include "YHMacros.h"
+
+NS_YH_BEGIN
+
+
+/** Interface that defines how to clone an object */
+class Clonable
+{
+public:
+	/** returns a copy of the object */
+    virtual Clonable* clone() const = 0;
+    /**
+     * @js NA
+     * @lua NA
+     */
+	virtual ~Clonable() {};
+};
+
+/**
+ * 引用计数基类
+ * 注意和RefCount不能同时使用
+ */
+class Object
+{
+public:
+    /**
+     * Constructor
+     *
+     * The object's reference count is 1 after construction.
+     * @js NA
+     */
+    Object();
+    
+    /**
+     * @js NA
+     * @lua NA
+     */
+    virtual ~Object();
+    
+    /**
+     * Release the ownership immediately.
+     *
+     * This decrements the object's reference count.
+     *
+     * If the reference count reaches 0 after the descrement, this object is
+     * destructed.
+     *
+     * @see retain, autorelease
+     * @js NA
+     */
+    void release();
+
+    /**
+     * Retains the ownership.
+     *
+     * This increases the object's reference count.
+     *
+     * @see release, autorelease
+     * @js NA
+     */
+    inline void retain()
+    {
+        ++_referenceCount;
+    }
+
+    unsigned int getReferenceCount() const;
+
+    /**
+     * Returns a boolean value that indicates whether this object and a given
+     * object are equal.
+     *
+     * @param object    The object to be compared to this object.
+     *
+     * @returns True if this object and @p object are equal, otherwise false.
+     * @js NA
+     * @lua NA
+     */
+    virtual bool isEqual(const Object* object);
+
+public:
+	
+	inline void setId(unsigned int id)
+	{
+	    _id = id;
+	}
+
+	inline unsigned int getId()
+	{
+	    return _id;
+	}
+	
+protected:
+    /// object id
+    unsigned int _id;
+    /// count of references
+    unsigned int _referenceCount;
+};
+
+NS_YH_END
+
+#endif // YH_BASE_OBJECT_H_

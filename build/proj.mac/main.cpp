4eaf59a5db07d55393b63d55e1defa985442572e
trarck <trarck@gmail.com>
2014-08-19 11:27:37 +0800

    测试List

--- build/proj.mac/main.cpp
+++ build/proj.mac/main.cpp
@@ -13,8 +13,19 @@ using namespace yh::cli::detail;
 int main(int argc, const char * argv[])
 {
     
+    typedef ds::LinkedList<int> List;
     
+    List a;
     
+    a.push_back(1);
+    a.push_back(2);
+    
+    List b;
+    b=a;
+    
+    printf("b:%ld",a.size());
+    
+    printf("\n");
     return 0;
 }
 

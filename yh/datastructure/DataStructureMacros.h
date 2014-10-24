8bf5eb0664dc7894234512787afb76477657e34a
trarck <trarck@gmail.com>
2014-10-24 16:30:32 +0800

    调整名称

--- yh/datastructure/DataStructureMacros.h
+++ yh/datastructure/DataStructureMacros.h
@@ -5,6 +5,6 @@
 
 #define NS_YH_DS_BEGIN                     namespace yh { namespace ds {
 #define NS_YH_DS_END                       }}
-#define USING_NS_DS_JSON                    using namespace yh::ds
+#define USING_NS_YH_DS                    using namespace yh::ds
 
 #endif //YH_DATASTRUCTURE_DSMACROS_H_

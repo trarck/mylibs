db7af8b21c3fd7c121bcff51ca9b8fad8f8d4c13
trarck <trarck@gmail.com>
2016-02-22 14:54:52 +0800

    add generate android

--- build.bat
+++ build.bat
@@ -4,5 +4,8 @@ gyp --depth=. -Dlibrary=static_library --generator-output=build/win32
 rem ios
 gyp --depth=. -Dlibrary=static_library -f xcode -DOS=ios --generator-output=build/ios
 
-rem ios
+rem mac
 gyp --depth=. -Dlibrary=static_library -f xcode -DOS=mac --generator-output=build/mac
+
+rem android
+gyp --depth=. -Dlibrary=static_library -f android -DOS=android -Gandroid_top_dir=./

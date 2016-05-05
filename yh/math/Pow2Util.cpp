217f9dd986d161b1bd8453694bc9c81965ec8859
trarck <trarck@gmail.com>
2016-05-05 23:24:26 +0800

    add math utils

---
+++ yh/math/Pow2Util.cpp
@@ -0,0 +1,50 @@
+#include "Pow2Util.h"
+#include <math.h>
+
+NS_YH_BEGIN
+
+bool isPow2(int n)
+{
+	if (n <= 0)
+		return false;
+	return (n & (n - 1)) == 0;
+}
+
+// Next power of 2  
+int nextPow2(int n)
+{
+	n--;
+	n |= n >> 1;
+	n |= n >> 2;
+	n |= n >> 4;
+	n |= n >> 8;
+	n |= n >> 16;
+	n++;
+	return n;
+}
+
+// Previous power of 2  
+int prevPow2(int n)
+{
+	int n2 = n >> 1;
+	// divide by 2    
+	if (n2 > 0)
+		return nextPow2(n2);
+	else
+		return nextPow2(n);
+}
+
+// Nearest power of 2  
+int nearPow2(int n)
+{
+	if (isPow2(n))
+		return n;
+	int nNext = nextPow2(n);
+	int nPrev = prevPow2(n);
+	if (nNext - n > abs(n - nPrev))
+		return nPrev;
+	else
+		return nNext;
+}
+
+NS_YH_BEGIN
---
+++ yh/math/Pow2Util.h
@@ -0,0 +1,21 @@
+#ifndef YH_POW2UTIL_H_
+#define YH_POW2UTIL_H_
+
+#include "../YHMacros.h"
+
+NS_YH_BEGIN
+
+bool isPow2(int n);
+
+// Next power of 2  
+int nextPow2(int n);
+
+// Previous power of 2  
+int prevPow2(int n);
+
+// Nearest power of 2  
+int nearPow2(int n);
+
+NS_YH_BEGIN
+
+#endif //YH_POW2UTIL_H_

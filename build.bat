b906ae864a74e830560149658ffd4a00cdae3053
trarck <trarck@gmail.com>
2017-03-29 17:19:35 +0800

    修改build

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
 rem win
-gyp --depth=. -Dyh_librarylibrary=static_library --generator-output=build/win32
+gyp --depth=. -Dyh_library=static_library --generator-output=build/win32
 
 rem ios
-gyp --depth=. -Dyh_librarylibrary=static_library -f xcode -DOS=ios --generator-output=build/ios
+gyp --depth=. -Dyh_library=static_library -f xcode -DOS=ios --generator-output=build/ios
 
 rem mac
-gyp --depth=. -Dyh_librarylibrary=static_library -f xcode -DOS=mac --generator-output=build/mac
+gyp --depth=. -Dyh_library=static_library -f xcode -DOS=mac --generator-output=build/mac
 
 rem android
-gyp --depth=. -Dyh_librarylibrary=static_library -f android -DOS=android -Gandroid_top_dir=./
+gyp --depth=. -Dyh_library=static_library -f android -DOS=android -Gandroid_top_dir=./
 
 rem cmake
 build\gyp\gyp --depth=. -Dyh_library=static_library -f cmake-android -DOS=android -Gandroid_top_dir=./  --generator-output=build\cmake

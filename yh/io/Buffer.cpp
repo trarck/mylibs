b8e47a366f7679fe6669b3038afc069ce7be73cd
trarck <trarck@gmail.com>
2017-03-30 14:35:33 +0800

    加入io的子命名空间。

--- yh/io/Buffer.cpp
+++ yh/io/Buffer.cpp
@@ -1,7 +1,7 @@
 #include "Buffer.h"
 #include <string>
 
-NS_YH_BEGIN
+NS_YH_IO_BEGIN
 
 Buffer::Buffer()
 :m_size(0)
@@ -56,25 +56,24 @@ size_t Buffer::readBytes(size_t position,void* buf,size_t size)
     if (position+size>m_size) {
         size=m_size-position;
     }
-    
-    memmove(buf,m_data+position,size);
+    //通常buf和m_data不在同一内存块内，可以使用memcpy
+	memcpy(buf,m_data+position,size);
     
     return size;
 }
 
+size_t Buffer::readBytesSafe(size_t position, void* buf, size_t size)
+{
+	YHASSERT(position + size <= m_size, "Buffer::readBytesSafe out index");
 
-//size_t Buffer::readBytesUnSafe(size_t position,void* buf,size_t size)
-//{
-//    YHASSERT(position+size<=m_size,"Buffer::readBytesSafe out index");
-//    
-////    if (position+size>m_size) {
-////        size=m_size-position;
-////    }
-//    
-//    memcpy(buf,m_data+position,size);
-//    
-//    return size;
-//}
+	if (position + size > m_size) {
+		size = m_size - position;
+	}
+
+	memmove(buf, m_data + position, size);
+
+	return size;
+}
 
 uint64_t Buffer::readUInt64LE(size_t position)
 {
@@ -90,6 +89,47 @@ uint64_t Buffer::readUInt64LE(size_t position)
     }
 
     return val;
+}
+
+uint32_t Buffer::readUInt32LE(size_t position)
+{
+	YHASSERT(position + YH_IO_INT_SIZE <= m_size, "Buffer::readUInt32LE out index");
+
+	unsigned char* start = m_data + position;
+
+	uint32_t val = (uint32_t)*(start + 3) << 24;
+	val |= (uint32_t)*(start + 2) << 16;
+	val |= (uint32_t)*(start + 1) << 8;
+	val |= *(start);
+
+	return val;
+}
+
+//使用字节转换来读取数据，比直接计算要慢20%左右。性能估计还是消耗在memcpy上。
+//所以还是直接使用数值运算快
+//uint32_t Buffer::readUInt32LE2(size_t position)
+//{
+//	YHASSERT(position + YH_IO_INT_SIZE <= m_size, "Buffer::readUInt32BE out index");
+//
+//	unsigned char buf[4];
+//
+//	readBytes(position, buf, 4);
+//
+//	return *((uint32_t*)buf);
+//}
+
+uint32_t Buffer::readUInt32BE(size_t position)
+{
+	YHASSERT(position + YH_IO_INT_SIZE <= m_size, "Buffer::readUInt32BE out index");
+
+	unsigned char* start = m_data + position;
+
+	uint32_t val = (uint32_t)*(start) << 24;
+	val |= (uint32_t)*(start + 1) << 16;
+	val |= (uint32_t)*(start + 2) << 8;
+	val |= *(start + 3);
+
+	return val;
 }
 
 uint64_t Buffer::readUInt64BE(size_t position)
@@ -108,6 +148,50 @@ uint64_t Buffer::readUInt64BE(size_t position)
     return val;
 }
 
+float Buffer::readFloat16LE(size_t position)
+{
+	YHASSERT(position + YH_IO_SHORT_SIZE <= m_size, "Buffer::readFloat16LE out index");
+	//        uint16_t halfInt=readUInt16LE(position);
+	//        
+	//        uint32_t singleInt=0;
+	//        
+	//        //半精度转单精度
+	//        halfp2singlesTyped(&singleInt, &halfInt);
+	//        
+	//        return bitwise_cast<uint32_t,float>(singleInt);
+
+	uint16_t halfInt = readUInt16LE(position);
+
+	float val;
+
+	//半精度转单精度
+	halfp2singles(&val, &halfInt);
+
+	return val;
+}
+
+float Buffer::readFloat16BE(size_t position)
+{
+	YHASSERT(position + YH_IO_SHORT_SIZE <= m_size, "Buffer::readFloat16BE out index");
+	//        uint16_t halfInt=readUInt16BE(position);
+	//        
+	//        uint32_t singleInt=0;
+	//        
+	//        //半精度转单精度
+	//        halfp2singlesTyped(&singleInt, &halfInt);
+	//                
+	//        return bitwise_cast<uint32_t,float>(singleInt);
+
+	uint16_t halfInt = readUInt16BE(position);
+
+	float val;
+
+	//半精度转单精度
+	halfp2singles(&val, &halfInt);
+
+	return val;
+}
+
 size_t Buffer::writeBytes(size_t position,void* buf,size_t size)
 {
     YHASSERT(position<m_size,"Buffer::writeBytes out index");
@@ -116,51 +200,103 @@ size_t Buffer::writeBytes(size_t position,void* buf,size_t size)
         size=m_size-position;
     }
     
-    memmove(m_data+position,buf,size);
+	memcpy(m_data+position,buf,size);
     
     return size;
 }
 
-size_t Buffer::writeBytesUnSafe(size_t position,void* buf,size_t size)
+size_t Buffer::writeBytesSafe(size_t position,void* buf,size_t size)
 {
     YHASSERT(position+size<=m_size,"Buffer::writeBytes out index");
     
-//    if (position+size>m_size) {
-//        size=m_size-position;
-//    }
+	if (position + size > m_size) {
+		size = m_size - position;
+	}
     
-    memcpy(m_data+position,buf,size);
+	memmove(m_data+position,buf,size);
     
     return size;
+}
+
+size_t Buffer::writeUInt16LE(uint16_t value, size_t position)
+{
+	YHASSERT(position + YH_IO_SHORT_SIZE <= m_size, "Buffer::writeUInt16LE out index");
+
+	unsigned char* start = m_data + position;
+
+	*(start) = value & 0x00FF;
+	*(start + 1) = (value & 0xFF00) >> 8;
+
+	return YH_IO_SHORT_SIZE;
+}
+
+size_t  Buffer::writeUInt16BE(uint16_t value, size_t position)
+{
+	YHASSERT(position + YH_IO_SHORT_SIZE <= m_size, "Buffer::writeUInt16BE out index");
+
+	unsigned char* start = m_data + position;
+
+	*(start) = (value & 0xFF00) >> 8;
+	*(start + 1) = value & 0x00FF;
+
+	return YH_IO_SHORT_SIZE;
+}
+
+size_t Buffer::writeUInt32LE(uint32_t value, size_t position)
+{
+	YHASSERT(position + YH_IO_INT_SIZE <= m_size, "Buffer::writeUInt32LE out index");
+
+	unsigned char* start = m_data + position;
+
+	*(start) = value & 0xFF;
+	*(start + 1) = (value >> 8) & 0xFF;
+	*(start + 2) = (value >> 16) & 0xFF;
+	*(start + 3) = (value >> 24) & 0xFF;
+
+	return YH_IO_INT_SIZE;
+}
+
+size_t Buffer::writeUInt32BE(uint32_t value, size_t position)
+{
+	YHASSERT(position + YH_IO_INT_SIZE <= m_size, "Buffer::writeUInt32BE out index");
+
+	unsigned char* start = m_data + position;
+
+	*(start) = (value >> 24) & 0xFF;
+	*(start + 1) = (value >> 16) & 0xFF;
+	*(start + 2) = (value >> 8) & 0xFF;
+	*(start + 3) = value & 0xFF;
+
+	return YH_IO_INT_SIZE;
 }
 
 size_t Buffer::writeUInt64LE(uint64_t value,size_t position)
 {
-    YHASSERT(position+_BUFFER_LONG_SIZE<=m_size,"Buffer::writeUInt64LE out index");
+    YHASSERT(position+YH_IO_LONG_SIZE<=m_size,"Buffer::writeUInt64LE out index");
     
     
     unsigned char* start=m_data+position;
     
-    for (int i = 0; i < _BUFFER_LONG_SIZE; i++)
+    for (int i = 0; i < YH_IO_LONG_SIZE; i++)
     {
         *(start+i) =  (value >> (i*8)) & 0xFF;
     }
     
-    return _BUFFER_LONG_SIZE;
+    return YH_IO_LONG_SIZE;
 }
 
 size_t Buffer::writeUInt64BE(uint64_t value,size_t position)
 {
-    YHASSERT(position+_BUFFER_LONG_SIZE<=m_size,"Buffer::writeUInt64LE out index");
+    YHASSERT(position+YH_IO_LONG_SIZE<=m_size,"Buffer::writeUInt64LE out index");
     
     unsigned char* start=m_data+position+7;
     
-    for (int i = 0; i < _BUFFER_LONG_SIZE; i++)
+    for (int i = 0; i < YH_IO_LONG_SIZE; i++)
     {
         *(start--) =  (value >> (i*8)) & 0xFF;
     }
     
-    return _BUFFER_LONG_SIZE;
+    return YH_IO_LONG_SIZE;
 }
 
 void Buffer::fill(unsigned char value,size_t start,size_t end)
@@ -245,4 +381,4 @@ void Buffer::setData(unsigned char* data, size_t size,bool dataOwner)
 	}
 }
 
-NS_YH_END
+NS_YH_IO_END
--- yh/io/IOMacros.h
+++ yh/io/IOMacros.h
@@ -1,5 +1,23 @@
 #ifndef YH_IO_BINARYMACROS_H_
 #define YH_IO_BINARYMACROS_H_
 
+#ifdef __cplusplus
+
+#define NS_YH_IO_BEGIN                     namespace yh { namespace io {
+#define NS_YH_IO_END                       }}
+#define USING_NS_YH_IO                     using namespace yh::io
+
+#else
+
+#define NS_YH_BEGIN                     
+#define NS_YH_END                       
+#define USING_NS_YH
+
+#endif
+
+#define YH_IO_BYTE_SIZE 1
+#define YH_IO_SHORT_SIZE 2
+#define YH_IO_INT_SIZE 4
+#define YH_IO_LONG_SIZE 8
 
 #endif //YH_IO_BINARYMACROS_H_
